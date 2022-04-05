918868ace29321d4279701d79deaea80f89a3757
AjaSharma93 <aja.sharma1101@gmail.com>
2022-04-05 17:50:50 +0100

    changed the display of the pokemon card

--- src/components/PokemonDialog.tsx
+++ src/components/PokemonDialog.tsx
@@ -4,35 +4,39 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
 
 export interface IPokemonResult {
-    status: number;
-    name: string;
-    description: string;
-    sprite: string;
-  }
+  status: number;
+  name: string;
+  description: string;
+  sprite: string;
+}
 
-  interface IPokemonResultProps{
-      pokemonInfo:IPokemonResult
-  }
+interface IPokemonResultProps {
+  pokemonInfo: IPokemonResult
+}
 
-  export function PokemonDialog({pokemonInfo}:IPokemonResultProps) {
-    return (<>
-    <Card sx={{ maxWidth: 400 }} style={{margin:"0 auto"}}>
+export function PokemonDialog({ pokemonInfo }: IPokemonResultProps) {
+  return (<>
+    <br/>
+    <Card sx={{ display: 'flex', maxWidth:500 }}>
       <CardMedia data-testid="pokemon_image"
         component="img"
         alt={pokemonInfo.name}
-        height="auto"
+        sx={{ maxWidth: 75 }}
         image={pokemonInfo.sprite}
       />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div" data-testid="pokemon_name">
-          {pokemonInfo.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" data-testid="pokemon_description">
-          {pokemonInfo.description}
-        </Typography>
-      </CardContent>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" data-testid="pokemon_name">
+            {pokemonInfo.name.toUpperCase()}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" data-testid="pokemon_description">
+            {pokemonInfo.description}
+          </Typography>
+        </CardContent>
+      </Box>
     </Card>
-    </>)
+  </>)
 }
